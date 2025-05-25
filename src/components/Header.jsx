6468bbe2009bc236ef6bd1d4f0f5ec00c977a726
Milagros <milagros_gordillo01@hotmail.com>
2025-05-25 01:36:08 -0300

    Landing Page en React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 export default function Header() {
   return (
-    <header className="grid grid-cols-3 items-center p-4 bg-gradient-to-r from-pink-900 to-black">
+    <header className="flex flex-col sm:flex-row sm:justify-between items-center p-4 bg-gradient-to-r from-pink-900 to-black">
       <img
         src="/lisa-rockstar.gif"
         alt="LISA Logo"
-        className="h-12 w-auto md:scale-200 scale-100 transform origin-left justify-self-start"
+        className="h-12 w-auto mb-2 sm:mb-0"
       />
 
-      <nav className="space-x-6 text-white justify-self-center text-xl sm:text-2xl md:text-3xl">
+      <nav className="flex flex-col sm:flex-row gap-2 sm:gap-6 text-white text-xl sm:text-2xl md:text-3xl items-center">
         <a href="#" className="hover:text-pink-400">Inicio</a>
         <a href="#" className="hover:text-pink-400">Video Musicales</a>
         <a href="#" className="hover:text-pink-400">Shop</a>
       </nav>
     </header>
-  )
+  );
 }
